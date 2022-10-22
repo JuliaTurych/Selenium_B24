f685850334949c62260909dab121323830659493
JuliaTurych <julia.turich@gmail.com>
2022-10-22 01:45:50 -0500

    new Task

---
+++ src/test/java/com/cybertek/tests/locators_intro/TC6_BankHeaderVerification.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.locators_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC6_BankHeaderVerification {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+String url = "https://www.zero.webappsecurity"
+        driver.get();
+
+
+
+    }
+}
